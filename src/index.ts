11714b189ea7b4dce3c0f6c60648cb14ac43c2d8
dnice <dnice_bo@Outlook.com>
2019-08-15 16:29:23 +0800

    entity

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,26 @@
-console.log('Hello NEM2-JZ-TS start !');
+import { IdentityContract } from './model/jz/IdentityContract';
+import { ApostilleData } from './model/jz/ApostilleData';
+
 export * from './infrastructure/infrastructure';
 export * from './model/model';
 export * from './service/service';
 export * from './core/utils/utility';
 export * from './core/format';
 export * from './core/crypto';
-console.log('Hello NEM2-JZ-TS end !');
+
+start();
+
+async function start() {
+
+    // const identity = new IdentityContract();
+    // const apostilleData = new ApostilleData();
+    // identity.requestIP = "127.0.0.1";
+    // identity.requestId = "0001";
+    // apostilleData.fxppbgs = "123123123";
+    // apostilleData.gpzjxbah = "asdzxc";
+    // apostilleData.license = "xxxx";
+    // identity.accountId = "qqwewewe";
+    // identity.ApostilleData = apostilleData;
+    // console.info(await IdentityContract.toStr(identity))
+}
+
