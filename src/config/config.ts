b50a07d000e0c415f446ba25a2beb7ee57433f14
dnice <dnice_bo@Outlook.com>
2019-10-10 09:24:08 +0800

    update g hash

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,7 +2,7 @@ import {NetworkType, UInt64} from "nem2-sdk";
 
 export const apiUrl = "http://47.108.88.254:3000";
 
-export const generationHash = "9F1979BEBA29C47E59B40393ABB516801A353CFC0C18BC241FEDE41939C907E7";
+export const generationHash = "27BB10AF8899BC74E0E823AF702838FF4399E7F7C7DCF6F3CFC6C466CB28DFF6";
 
 export const netType = NetworkType.MAIN_NET;
 
