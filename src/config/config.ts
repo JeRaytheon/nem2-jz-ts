81b674a0c228859f855262f4d6b01be5853ebb5c
dnice <dnice_bo@Outlook.com>
2019-10-10 14:52:18 +0800

    update netype

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,6 +4,6 @@ export const apiUrl = "http://47.108.88.254:3000";
 
 export const generationHash = "27BB10AF8899BC74E0E823AF702838FF4399E7F7C7DCF6F3CFC6C466CB28DFF6";
 
-export const netType = NetworkType.MAIN_NET;
+export const netType = NetworkType.MIJIN_TEST;
 
 export const fee = UInt64.fromUint(5)
--- test/api/HttpService.spec.ts
+++ test/api/HttpService.spec.ts
@@ -11,7 +11,7 @@ describe('service', () => {
             private: "64511ECAB0F03F1182F4F1C58E542BAA68A26D1ECFF85791CAF89128D3325E55"
         }
         const identityAccount = {
-            private: "9CE882FAD5480772ED00ABEE33DC0F7D379FFB802B1DD7A942C7EEAA5569FF87"
+            private: "22C11EBE4D5ABE56EAE2EED4D546930FAB413065189E983D32DC7ADD0CD14FC3"
         }
         const testAccount = Account.createFromPrivateKey(test.private, netType)
         const identityData = new IdentityModel();
@@ -31,9 +31,9 @@ describe('service', () => {
         }
         const testAccount = Account.createFromPrivateKey(test.private, netType)
         const account = {
-            private: "9CE882FAD5480772ED00ABEE33DC0F7D379FFB802B1DD7A942C7EEAA5569FF87"
+            private: "22C11EBE4D5ABE56EAE2EED4D546930FAB413065189E983D32DC7ADD0CD14FC3"
         }
-        const service = await HttpService.transactionService(apiUrl,account.private, testAccount.address.plain(), 1,"test");
+        const service = await HttpService.transactionService(apiUrl,account.private, testAccount.address.plain(), 123,"test");
         console.info(service);
     });
 
